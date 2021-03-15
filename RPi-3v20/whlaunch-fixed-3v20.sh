4df2a6d4df9dd4c805fcb2332ea80fc2514f9305
davecrump <dave.g8gkq@gmail.com>
2021-03-15 10:23:03 +0000

    202103140 Laucher Corrections

--- RPi-3v20/whlaunch-fixed-3v20.sh
+++ RPi-3v20/whlaunch-fixed-3v20.sh
@@ -6,5 +6,6 @@ whlaunch-fixed-3v20.sh
 # no VLC windows are started on the RPi
 # the final zero is the interface IP address which should be set if the RPi has more than one network interface
 
+cd /home/pi/winterhill/RPi-3v20
 ./winterhill-fixed-3v20.sh 192.168.77.237 9900 0
 
